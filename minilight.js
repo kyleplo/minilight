9d60bec321bb78505e3918dbd913fa471f0051ee
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-08-25 08:01:13 -0400

    Update and rename microlight.js to minilight.js

--- minilight.js
+++ minilight.js
@@ -19,16 +19,13 @@
     }
 }(this, function (exports) {
     // for better compression
-    var _window       = window,
-        _document     = document,
-        appendChild   = 'appendChild',
-        test          = 'test',
+    var _window          = window,
+        _document        = document,
+        appendChild      = 'appendChild',
+        test             = 'test',
         // style and color templates
-        textShadow    = ';text-shadow:',
-        opacity       = 'opacity:.',
-        _0px_0px      = ' 0px 0px ',
-        _3px_0px_5    = '3px 0px 5',
-        brace         = ')',
+        color            = ';color:',
+        minilightOptions = ["#222","#F50","#000","#06F","#E80","#930","#930","#6F6","#888","#6F6","#6F6"],
 
         i,
         microlighted,
@@ -38,7 +35,7 @@
     
     var reset = function(cls) {
         // nodes to highlight
-        microlighted = _document.getElementsByClassName(cls||'microlight');
+        microlighted = _document.getElementsByClassName(cls||'minilight');
 
         for (i = 0; el = microlighted[i++];) {
             var text  = el.textContent,
@@ -69,13 +66,6 @@
                 // flag determining if token is multi-character
                 multichar,
                 node,
-
-                // calculating the colors for the style templates
-                colorArr = /(\d*\, \d*\, \d*)(, ([.\d]*))?/g.exec(
-                    _window.getComputedStyle(el).color
-                ),
-                pxColor = 'px rgba('+colorArr[1]+',',
-                alpha = colorArr[3]||1;
 
             // running through characters and highlighting
             while (prev2 = prev1,
@@ -120,38 +110,7 @@
                         // (some types are highlighted similarly)
                         el[appendChild](
                             node = _document.createElement('span')
-                        ).setAttribute('style', [
-                            // 0: not formatted
-                            '',
-                            // 1: keywords
-                            textShadow + _0px_0px+9+pxColor + alpha * .7 + '),' +
-                                         _0px_0px+2+pxColor + alpha * .4 + brace,
-                            // 2: punctuation
-                            opacity + 6 +
-                            textShadow + _0px_0px+7+pxColor + alpha / 4 + '),' +
-                                         _0px_0px+3+pxColor + alpha / 4 + brace,
-                            // 3: strings and regexps
-                            opacity + 7 +
-                            textShadow + _3px_0px_5+pxColor + alpha / 5 + '),-' +
-                                         _3px_0px_5+pxColor + alpha / 5 + brace,
-                            // 4: comments
-                            'font-style:italic;'+
-                            opacity + 5 +
-                            textShadow + _3px_0px_5+pxColor + alpha / 4 + '),-' +
-                                         _3px_0px_5+pxColor + alpha / 4 + brace
-                        ][
-                            // not formatted
-                            !tokenType ? 0 :
-                            // punctuation
-                            tokenType < 3 ? 2 :
-                            // comments
-                            tokenType > 6 ? 4 :
-                            // regex and strings
-                            tokenType > 3 ? 3 :
-                            // otherwise tokenType == 3, (key)word
-                            // (1 if regexp matches, 0 otherwise)
-                            + /^(a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto)|b(ase|egin|ool(ean)?|reak|yte)|c(ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|de(bugger|cimal|clare|f(ault|er)?|init|l(egate|ete)?)|do|double|e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|f(allthrough|alse|inal(ly)?|ixed|loat|or(each)?|riend|rom|unc(tion)?)|global|goto|guard|i(f|mp(lements|licit|ort)|n(it|clude(_once)?|line|out|stanceof|t(erface|ernal)?)?|s)|l(ambda|et|ock|ong)|m(icrolight|odule|utable)|NaN|n(amespace|ative|ext|ew|il|ot|ull)|o(bject|perator|r|ut|verride)|p(ackage|arams|rivate|rotected|rotocol|ublic)|r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn))|s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of))|u(n(checked|def(ined)?|ion|less|signed|til)|se|sing)|v(ar|irtual|oid|olatile)|w(char_t|hen|here|hile|ith)|xor|yield)$/[test](token)
-                        ]);
+                        ).setAttribute('style', minilightOptions[/^(a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto)|b(ase|egin|ool(ean)?|reak|yte)|c(ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|de(bugger|cimal|clare|f(ault|er)?|init|l(egate|ete)?)|do|double|e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|f(allthrough|alse|inal(ly)?|ixed|loat|or(each)?|riend|rom|unc(tion)?)|global|goto|guard|i(f|mp(lements|licit|ort)|n(it|clude(_once)?|line|out|stanceof|t(erface|ernal)?)?|s)|l(ambda|et|ock|ong)|m(icrolight|odule|utable)|NaN|n(amespace|ative|ext|ew|il|ot|ull)|o(bject|perator|r|ut|verride)|p(ackage|arams|rivate|rotected|rotocol|ublic)|r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn))|s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of))|u(n(checked|def(ined)?|ion|less|signed|til)|se|sing)|v(ar|irtual|oid|olatile)|w(char_t|hen|here|hile|ith)|xor|yield)$/[test](token) ? 3 : tokenType]);
 
                         node[appendChild](_document.createTextNode(token));
                     }
